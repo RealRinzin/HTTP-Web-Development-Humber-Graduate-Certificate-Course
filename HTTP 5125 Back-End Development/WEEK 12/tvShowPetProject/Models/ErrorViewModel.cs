c61ca4683c5bead747ce966e2ba55d0337e7f078
Rinzin <geniusbulbs@gmail.com>
2026-04-23 13:24:36 -0400

    backend update

--- HTTP 5125 Back-End Development/WEEK 12/tvShowPetProject/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace tvShowPetProject.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
